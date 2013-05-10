5edaae2f833667b36387426af7ec8a79d44fdf22
Jonathan Jin <jjin082693@gmail.com>
2013-05-10 12:03:04 -0500

    completed pe42.py

--- pe42.py
+++ pe42.py
@@ -2,6 +2,8 @@ import time
 
 time_start = time.time()
 
+################################################################################
+#### Methods ####
 def wordToNum(word):
     sum = 0
 
@@ -10,21 +12,34 @@ def wordToNum(word):
 
     return sum
 
-def wordIsTri(word):
-    # TODO
-    return True
+def isTri(word):
+    wordNum = wordToNum(word)
+    for num in tris:
+        if (wordNum == num):
+            return True
+    return False
 
-def generateTris():
-    tris = []
-    
-
+################################################################################
+#### Items ####
 words = [word.strip('"\n').upper() for word in
          open("words.txt",'r').read().split(',')]
 
-wordSums = [wordToNum(word) for word in words]
 
-tris = [] # TODO
+tris = []
+n = 1
+while  0.5*n*(n+1) < max([wordToNum(word) for word in words]):
+    tris.append(int(0.5*n*(n+1)))
+    n+=1
+    
 
-print words
-print wordSums
+################################################################################
+#### Procession ####
+
+quant = 0
+for word in words:
+    if (isTri(word)):
+        quant+=1
+
+print quant
 print "Runtime:",time.time()-time_start,"seconds"
+
